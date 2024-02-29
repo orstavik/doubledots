7a0b0117296aaf789d2733aec7974efbb2368f8b
MARTIN CASAS <mirpocasas@gmail.com>
2024-02-29 13:25:12 -0300

    testing

--- elementObserver2.js
+++ elementObserver2.js
@@ -1,19 +1,28 @@
 import { domEvents, windowEvents, documentEvents } from "./test/scriptBlocker/nativeEvents.js";
 
 const elEvents = [...domEvents()];
-const regx = elEvents.map(n => n + ":+").join("|");
+const regx = elEvents.map((n) => n + ":+").join("|");
 const docEvents = [...documentEvents()];
 const winEvents = [...windowEvents()];
 
+function getCustomReactionsFromElements(elementsArr) {
+  let customReactions = [];
+  console.log(elementsArr);
+  for (let element in elementsArr) {
+    console.log(element.getAttributeNames());
+    console.log(element.outerHTML);
+    console.log(element.nodeName);
+  }
+  return customReactions;
+}
+
 function doDispatch(elems) {
-  const customReactions = getCustomReactionsFromElements(newElements);
+  const customReactions = getCustomReactionsFromElements(elems);
   //todo this is the callback
-  console.log(...customReactions);
 }
 
 (function () {
-  function monkeyPatch(proto, prop, valueOrSet, hoFun,
-    desc = Object.getOwnPropertyDescriptor(proto, prop)) {
+  function monkeyPatch(proto, prop, valueOrSet, hoFun, desc = Object.getOwnPropertyDescriptor(proto, prop)) {
     desc[valueOrSet] = hoFun(desc[valueOrSet]);
     Object.defineProperty(proto, prop, desc);
   }
@@ -25,7 +34,7 @@ function doDispatch(elems) {
   function innerHTML_ho(og) {
     return function innerHTML_patch(val) {
       og.call(this, val);
-      doDispatch([...this.querySelectorAll('*')]);
+      doDispatch([...this.querySelectorAll("*")]);
     };
   }
 
@@ -38,41 +47,55 @@ function doDispatch(elems) {
   monkeyPatch(ShadowRoot.prototype, "innerHTML", "set", innerHTML_ho);
   monkeyPatch(Element.prototype, "insertAdjacentHTML", "value", function (og) {
     return function insertAdjacentHTML_patch(position, ...args) {
-      const root = position === 'beforebegin' || position === 'afterend' ? this.parentNode : this;
+      const root = position === "beforebegin" || position === "afterend" ? this.parentNode : this;
       const index = root.children.indexOf(this);
       const length = root.children.length;
       og.call(this, position, ...args);
       const length2 = root.children.length;
       const added = length2 - length;
       const newRoots = root.children.slice(index, length);
-      doDispatch(newRoots.map(r => r.querySelectorAll("*")).flatten());
+      doDispatch(newRoots.map((r) => r.querySelectorAll("*")).flatten());
     };
   });
 })();
 
 function pauseScriptInMRS(mrs) {
   const last = mrs[mrs.length - 1].target;
-  if (last.tagName === "SCRIPT")
-    pauseScript(last);
+  if (last.tagName === "SCRIPT") pauseScript(last);
 }
 
 function pauseScript(script) {
-  if (script.hasAttribute("src"))
-    script.setAttribute(":src", script.getAttribute("src"));
+  if (script.hasAttribute("src")) script.setAttribute(":src", script.getAttribute("src"));
   script.setAttributeNode(document.createAttribute("src"));
 }
 
 function newElementsFromMrs(mrs) {
-  throw "implement me, get all the added elements from all the mr in mrs. Please. Thank you)";
+  let response = [];
+  mrs.map((mr) => {
+    if (mr.addedNodes.length > 0) {
+      mr.addedNodes.forEach((element) => {
+        response = response.concat(element);
+      });
+    }
+    if (mr.removedNodes.length > 0) {
+      mr.removedNodes.forEach((element) => {
+        response = response.concat(element);
+      });
+    }
+  });
+  return response;
+  // throw "implement me, get all the added elements from all the mr in mrs. Please. Thank you)";
 }
 
-document.addEventListener("beforescriptexecute", e => e.preventDefault());
-const mo = new MutationObserver(function(mrs) {
+document.addEventListener("beforescriptexecute", (e) => e.preventDefault());
+const mo = new MutationObserver(function (mrs) {
   pauseScriptInMRS(mrs);
   const newElements = newElementsFromMrs(mrs);
   doDispatch(newElements);
-  if(document.readyState === "complete") //interactive??
+  if (document.readyState === "complete")
+    //interactive??
     mo.disconnect();
+    
 });
 mo.observe(document.documentElement, { childList: true, subtree: true });
 
