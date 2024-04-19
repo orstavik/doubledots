57e4bf75f6fbb71ee07b113ff275a6c7582728ad
orstavik <orstavik77@gmail.com>
2024-04-19 10:04:50 +0200

    DoubleDots.nativeEvents and updating doc in simple prop

--- src/1_1_DoubleDots.js
+++ src/1_1_DoubleDots.js
@@ -5,7 +5,7 @@ window.DoubleDots = {
   DefinitionError: class DefinitionError extends Error { },
   MissingReaction: class MissingReaction extends Error { },
   DisconnectedError: class DisconnectedError extends Error { },
-  
+
   AttrWeakSet: class AttrWeakSet extends Set {
     static #bigSet = new Set(); //wr => AttrWeakSet
     static #key;
@@ -33,5 +33,28 @@ window.DoubleDots = {
       AttrWeakSet.#key ??= setInterval(AttrWeakSet.gc, AttrWeakSet.GC);
       super.add(at);
     }
-  }
+  },
+  nativeEvents: (function () {
+    function extractHandlers(obj) {
+      return Object.keys(obj)
+        .filter(k => k.startsWith("on"))
+        .map(k => k.substring(2).toLowerCase());
+    }
+    const onElement = extractHandlers(Element.prototype);
+    const onHTMLElement = extractHandlers(HTMLElement.prototype);
+    const onWindow = extractHandlers(window);
+    const onDocument = extractHandlers(Document.prototype);
+
+    const result = {
+      element: [...onElement, ...onHTMLElement].filter((a, i) => combined.indexOf(a) === i),
+      window: onWindow.filter(x => !element.includes(x)),
+      document: onDocument.filter(x => !element.includes(x) && !window.includes(x)),
+      //todo should I switch the difference between window and document?
+    };
+    result.element.push("touchstart", "touchmove", "touchend", "touchcancel");
+    result.document.push("DOMContentLoaded");
+    Object.values(result).forEach(Object.freeze);
+    Object.freeze(result);
+    return result;
+  })()
 };
--- src/propagationTriggers/1_simple_listeners.js
+++ src/propagationTriggers/1_simple_listeners.js
@@ -16,63 +16,74 @@ class DCLTrigger extends DocumentTrigger {
   }
 }
 
-//method 1. for registering all the native event types.
-//          as long as there is no higher-order functions, this should work fine.
-const nativeEventType = (function (HTMLElement_p, Element_p, Document_p) {
-  function extractHandlerNames(obj) {
-    return new Set(
-      Object.keys(obj)
-        .filter(k => k.startsWith("on"))
-        .map(k => k.substring(2).toLowerCase())
-    );
+//global pre-propagation listener: "_click"
+class PrePropTrigger extends WindowTrigger {
+  get type() {
+    return this.trigger.slice(1); //remove prefix "_click"
   }
+  get options() {
+    return true;
+  }
+}
 
-  let onElement = extractHandlerNames(Element_p);
-  const onHTMLElement = extractHandlerNames(HTMLElement_p);
-  const nonHandler = new Set([
-    "touchstart", "touchmove", "touchend", "touchcancel"
-  ]);
-  let onDocument = extractHandlerNames(Document_p);
-  let onWindow = extractHandlerNames(window);
+//global post-propagation listener: "click_"
+class PostPropTrigger extends WindowTrigger {
+  get type() {
+    return this.trigger.slice(-1); //remove postfix "click_"
+  }
+}
 
-  onElement = onElement.union(onHTMLElement).union(nonHandler);
-  onWindow = onWindow.difference(onElement);
-  onDocument = onDocument.difference(onElement).difference(onWindow);
+const nativeEventType = (function () {
 
-  for (let type of onElement)
+  for (let type of DoubleDots.nativeEvents.element) {
     document.Triggers.define(type, AttrListener);
-  for (let type of onWindow)
+    document.Triggers.define("_" + type, PrePropTrigger);
+    document.Triggers.define(type + "_", PostPropTrigger);
+  }
+  for (let type of DoubleDots.nativeEvents.window)
     document.Triggers.define(type, WindowTrigger);
-  for (let type of onDocument)
+  for (let type of DoubleDots.nativeEvents.document)
     document.Triggers.define(type, DocumentTrigger);
   document.Triggers.define("domcontentloaded", DCLTrigger);
 
-  //1. global element listeners: "_click" and "click_":
-  class GlobalPreElementTrigger extends WindowTrigger {
-    get type() {
-      return this.trigger.slice(1); //removes the prefix "_"
-    }
-    get options() {
-      return true;
-    }
-  }
+})();
 
-  class GlobalPostElementTrigger extends WindowTrigger {
-    get type() {
-      return this.trigger.slice(-1); //removes the postfix "_"
-    }
-  }
+/**
 
-  for (let type of onElement) {
-    document.Triggers.define("_" + type, GlobalPreElementTrigger);
-    document.Triggers.define(type + "_", GlobalPostElementTrigger);
-  }
+## HowTo: use `custom-event`s in this SimplePropagation setup?
 
-})(HTMLElement.prototype, Element.prototype, Document.prototype);
+To `dispatch` and listen for a `custom-event` using SimplePropagation, 
+you must first and only register their triggers:
 
-//2. custom events must be added as `AttrListener`.
-document.Triggers.define("my-event", AttrListener);
+1. If you only wish to have normal bubble triggers for them, you only register their bubble triggers.
+```js
+  document.Triggers.define("custom-event", AttrListener);  
+```
 
-//the problem here is that we struggle controlling the default action.
-//if we only want to use the preventDefault(), then this is not a problem.
-//but, if we need to add our own default actions, then this is a problem.
+2. If you wish the event to be a global, you register it as a WindowTrigger.
+```js
+  document.Triggers.define("custom-global-event", WindowTrigger);  
+```
+3. If you wish the event to be global, and you want to use pre- and post-propagation triggers, register all needed triggers.
+```js
+    document.Triggers.define("custom-event", AttrListener);
+    document.Triggers.define("_custom-event", PrePropTrigger);
+    document.Triggers.define("custom-event_", PostPropTrigger);
+```
+
+## DefaultActions, `stopPropagation()` and SimplePropagation
+
+The SimplePropagation setup enables you to use native default actions normally.
+This means that you can call `e.preventDefault()` within any reaction chain for that event.
+
+DoubleDots recommend *against* using `e.stopPropagation()` and `e.stopImmediatePropagation()`. But when you use SimplePropagation, you can allow stopPropagation().
+
+## Performance and consistency
+
+SimplePropagation follows the same logic as native event listeners. This means that:
+1. listeners/triggers can always be removed. This makes sense.
+2. But, *sometimes* listeners can be added and triggered during the same event propagation cycle, *sometimes* not. Listeners added later in the propagation cycle will be triggered, except when they are on the same element in the same phase as the current element.
+3. The sync/async behavior can be confusing, as sometimes each event listener is given its own macro task, sometimes all event listeners are considered *one* macro task, and sometimes the event listeners are considered sync (when dispatched via `dispatchEvent`).
+
+The overhead per native event listener is not that big. It is quite performant. The only cost is resolving the reaction chain and maintaining the eventLoop state.
+*/
