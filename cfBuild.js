c6b3182f984f940b0c842f265aa63334f3e1a083
orstavik <orstavik77@gmail.com>
2024-05-23 11:42:12 +0200

    build testing

--- cfBuild.js
+++ cfBuild.js
@@ -2,31 +2,16 @@ const fs = require('fs');
 const path = require('path');
 
 // Function to recursively generate the directory structure as a JSON object
-function generateDirectoryObject(dir) {
-  const result = {};
-  const items = fs.readdirSync(dir);
-  for (let item of items) {
-    const itemPath = path.join(dir, item);
-    const stats = fs.statSync(itemPath);
-
-    if (stats.isDirectory()) {
-      result[item] = generateDirectoryObject(itemPath);
-    } else {
-      result[item] = 1;
-    }
+function dirList(dir) {
+  const res = {};
+  for (let item of fs.readdirSync(dir)) {
+    const path = path.join(dir, item);
+    res[item] = fs.statSync(path).isDirectory() ? dirList(path) : 1;
   }
-  return result;
+  return res;
 }
 
-// Define the base directory
-// const baseDir = path.join(__dirname, '..'); // Adjust to the appropriate base directory
-// Generate the directory object
-const directoryObject = generateDirectoryObject(__dirname);
-
-// Define the output file path
-const outputPath = path.join(__dirname, 'hello.json');
-
-// Write the JSON object to hello.json
-fs.writeFileSync(outputPath, JSON.stringify(directoryObject, null, 2), 'utf8');
-
+const result = dirList(__dirname);
+const filename = path.join(__dirname, 'hello.json');
+fs.writeFileSync(filename, JSON.stringify(result, null, 2), 'utf8');
 console.log('hello sunshine.');
