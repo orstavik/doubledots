751fc6046513e5f490b0052e8c4cf879cd55e847
orstavik <orstavik77@gmail.com>
2024-10-21 19:30:33 +0200

    fixing ddx

--- .vscode/ddbb.sh
+++ .vscode/ddbb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 npx esbuild src/dd/dd.js src/dd/ddStrict.js --format=iife --target=es2018 --outdir=. --bundle --sourcemap --minify
-npx esbuild x/ddx.js --target=es2018 --outdir=. --bundle --sourcemap --minify 
+npx esbuild x/ddx.js --bundle --format=esm --outdir=. --tree-shaking=false --sourcemap --minify
 
 output_files=$(find . -maxdepth 1 -type f -name "*.js" -o -name "*.js.map")
 git add $output_files
