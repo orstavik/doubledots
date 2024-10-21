b6e335034a5dae9d515bdd056d01f4e8d506f9e6
orstavik <orstavik77@gmail.com>
2024-10-21 18:39:14 +0200

    new release

--- src/dd/dd.js
+++ src/dd/dd.js
@@ -2,4 +2,4 @@ import {} from "./1_DoubleDots.js";
 import {} from "./2_AttrCustom.js";
 import {} from "./3_definition_registers_v4.js";
 import {} from "./4_eventLoop_v2.js";
-import {} from "./6_load_DoubleDots.js";
+import {} from "./5_load_DoubleDots.js";
