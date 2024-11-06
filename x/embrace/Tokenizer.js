f819f658bae141afb0b46aa9d0a5e8aaa1bb81c9
orstavik <orstavik77@gmail.com>
2024-11-06 13:23:40 +0100

    babysteps embrace

--- x/embrace/Tokenizer.js
+++ x/embrace/Tokenizer.js
@@ -1,15 +1,20 @@
-// const illegals = /window|this|document|globalThis|arguments/;
-//the word are all references. They will *all* be replaced with arg[i]
-const dangerous = /super|this|globalThis|window|document/;
-const words = /#?[\p{L}_$][\p{L}\p{N}_$]*(?:\s*\.\s*[\p{L}_$][\p{L}\p{N}_$]*)*/u;
 const ignore = /\b(?:break|case|catch|class|const|continue|debugger|default|delete|do|else|enum|export|extends|false|finally|for|function|if|implements|import|in|instanceof|interface|let|new|null|package|private|protected|public|return|static|switch|throw|true|try|typeof|var|void|while|with|yield|async|await)\b/;
-const quote = /(["'])(?:\\.|(?!\1).)*\1/;
+const dotWords = /\.\s*[\p{L}_$][\p{L}\p{N}_$]*(?:\s*\.\s*[\p{L}_$][\p{L}\p{N}_$]*)*/u;
+const words = /#?[\p{L}_$][\p{L}\p{N}_$]*(?:\s*\.\s*[\p{L}_$][\p{L}\p{N}_$]*)*/u;
+const quote1 = /'([^'\\]*(\\.[^'\\]*)*)'/;
+const quote2 = /"([^"\\]*(\\.[^"\\]*)*)"/;
 const number = /0[xX][0-9a-fA-F]+|\d*\.?\d+(?:[eE][+-]?\d+)?/;
 const regex = /\/[^/\\]*(?:\\.[^/\\]*)*\/[gimyu]*/;
 const linecomment = /\/\/[^\n]*/;
 const starcomment = /\/\*[^]*?\*\//;
 
-const tokens = [ignore, words, quote, number, linecomment, starcomment, regex];
+//todo security problems.
+//1. template strings: `comments ${here can come devils}`. strategy 1) make it throw an error, 2) tokenize ${} inside recursively?..
+//2. something["bad"].CAN.HAPPEN.HERE.constructor.__proto__.etc strategy a) make it throw an error? b) make the function work hiddenly?
+// const dangerous = /super|window|this|document|globalThis|arguments|Function|eval/;
+// These dangerous words are captured, replaced with args[1], and attempted gotten from the context. Thus, they are safe.
+
+const tokens = [ignore, words, dotWords, quote1, quote2, number, linecomment, starcomment, regex];
 const tokenizer = new RegExp(tokens.map(r => `(${r.source})`).join("|"), "gu");
 
 export function extractArgs(txt, params = []) {
@@ -32,6 +37,7 @@ const tsts = [[
   const number = /n . a . m . e/;
   const regex = /\/[^/\\]*(?:\\.[^/\\]*)*\/[gimyu]*/;
   const starcomment = /\/\*[^]*?\*\//;`,
+
   `//the word are all references. They will *all* be replaced with arg[i]
   const args[0] = / #something.else */u;
   const args[1] = / name /;
@@ -39,8 +45,11 @@ const tsts = [[
   const args[3] = //[^/\\]*(?:\\.[^/\\]*)*/[gimyu]*/;
   const args[4] = //*[^]*?*//;`
 ], [
-`name hello . sunshine #hello.world bob123 _123`,
-`args[0] args[1] args[2] args[3] args[4]`
+  `name hello . sunshine #hello.world bob123 _123`,
+  `args[0] args[1] args[2] args[3] args[4]`
+], [
+  `name.hello["bob"].sunshine  . bob`,
+  `args[0]["bob"].sunshine  . bob`
 ]
 
 ];
