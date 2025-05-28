1f535c77510821f3207c1dd7ad477ac0202f00f4
orstavik <orstavik77@gmail.com>
2025-05-28 15:54:34 +0200

    making dotRule ?. instead of . and adding support for updated arguments i. i2, i0, etc.

--- ddx.js
+++ ddx.js
@@ -550,10 +550,28 @@ const scopes = {
   "t.": "window.eventLoop.event.target.",
   "w.": "window.",
   "d.": "window.document.",
-  // "i.": "args[0].",  //todo implement this instead of .oi
-  // "i(0-9)+": "args[$1].",//todo implement this instead of .oi
-  "oi.": "oi.",
-  "at.": "window.eventLoop.attribute.", //useful when dash rules have moved the origin
+
+  "i.": "args[0].",
+  "i0.": "args[0].",
+  "i1.": "args[1].",
+  "i2.": "args[2].",
+  "i3.": "args[3].",
+  "i4.": "args[4].",
+  "i5.": "args[5].",
+  "i6.": "args[6].",
+  "i7.": "args[7].",
+  "i8.": "args[8].",
+  "i9.": "args[9].",
+  "i10.": "args[10].",
+  "i11.": "args[11].",
+  "i12.": "args[12].",
+  "i13.": "args[13].",
+  "i14.": "args[14].",
+  "i15.": "args[15].",
+  "args": "args",
+
+  "oi.": "args[0].",
+  "at.": "window.eventLoop.attribute.", //useful when dash rules have moved the origin. dash rules break lob.. used in gestures?
   "el.": "window.eventLoop.attribute.ownerElement.", //todo same as this.ownerElement??
   "this.": "this.",
   "window.": "window.",
@@ -572,7 +590,7 @@ const primitives =
 
 function textToExp(txt) {
   let [prop, ...args] = txt.split("_");
-  const ref = processRef(prop);
+  const ref = processRef(prop).replaceAll(".", "?.");
   args = args.map(arg => processRef(arg) || primitives.test(arg) ? arg : `"${arg}"`);
   const sargs = args.join(", ");
   const setter = !args.length ? "" : args.length === 1 ? `=${sargs}` : `=[${sargs}]`;
@@ -581,7 +599,7 @@ function textToExp(txt) {
 
 function DotReactionRule(fullname) {
   const exp = textToExp(fullname);
-  const code = `function dotReaction(oi) { return ${exp}; }`;
+  const code = `function dotReaction(...args) { return ${exp}; }`;
   return DoubleDots.importBasedEval(code);
 }
 
