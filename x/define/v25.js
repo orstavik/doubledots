5656a2fcc7d82ab3ea4fbc60ac46f6d48151f5b0
orstavik <orstavik77@gmail.com>
2025-06-16 17:10:36 +0200

    update x/define

--- x/define/v25.js
+++ x/define/v25.js
@@ -47,62 +47,64 @@ async function loadDef(src, name) {
 const camelCase = str => str.replace(/-[a-z]/g, c => c[1].toUpperCase());
 const PascalCase = str => str.replace(/^(_*[a-z])|-([a-z])/g, (_, a, c = a) => c.toUpperCase());
 
-function defineReaction(name, module) {
+function define(register, name, Def) {
   if (!name.match(/^_*[a-z]([a-z0-9_.-]*[a-z])?$/))
     throw new SyntaxError(`Invalid definition name: "${name}". name.match(/^_*[a-z]([a-z0-9_.-]*[a-z])?$/)`);
+  register.define(name, Def);
+}
+
+function defineRule(register, name, Def) {
+  if (!name.match(/^_*[a-z][a-z0-9_.-]*[_.-]$/))
+    throw new SyntaxError(`Invalid rule name: "${name}". name.match(/^_*[a-z][a-z0-9_.-]*[_.-]$/)`);
+  register.defineRule(name, Def);
+}
+
+function defineReaction(name, module) {
   const { src, params } = srcAndParams(this, name, module);
   for (let [name, value] of params.entries())
-    this.getRootNode().Reactions.define(name, loadDef(src, value || camelCase(name)));
+    define(this.getRootNode().Reactions, name, loadDef(src, value || camelCase(name)));
 }
 
 function defineTrigger(name, module) {
-  if (!name.match(/^_*[a-z]([a-z0-9_.-]*[a-z])?$/))
-    throw new SyntaxError(`Invalid definition name: "${name}". name.match(/^_*[a-z]([a-z0-9_.-]*[a-z])?$/)`);
   const { src, params } = srcAndParams(this, name, module);
   for (let [name, value] of params.entries())
-    this.getRootNode().Triggers.define(name, loadDef(src, value || PascalCase(name)));
+    define(this.getRootNode().Triggers, name, loadDef(src, value || PascalCase(name)));
 }
 
 function defineReactionRule(name, module) {
-  if (!name.match(/^_*[a-z][a-z0-9_.-]*[_.-]$/))
-    throw new SyntaxError(`Invalid rule name: "${name}". name.match(/^_*[a-z][a-z0-9_.-]*[_.-]$/)`);
   const { src, params } = srcAndParams(this, name, module);
   for (let [name, value] of params.entries())
-    this.getRootNode().Reactions.defineRule(name, loadRuleDefs(src, value || camelCase(name)));
+    defineRule(this.getRootNode().Reactions, name, loadRuleDefs(src, value || camelCase(name)));
 }
 
 function defineTriggerRule(name, module) {
-  if (!name.match(/^_*[a-z][a-z0-9_.-]*[_.-]$/))
-    throw new SyntaxError(`Invalid rule name: "${name}". name.match(/^_*[a-z][a-z0-9_.-]*[_.-]$/)`);
   const { src, params } = srcAndParams(this, name, module);
   for (let [name, value] of params.entries())
-    this.getRootNode().Triggers.defineRule(name, loadRuleDefs(src, value || PascalCase(name)));
+    defineRule(this.getRootNode().Triggers, name, loadRuleDefs(src, value || PascalCase(name)));
 }
 
 function definePortal(name, module) {
-  if (!name.match(/^_*[a-z]([a-z0-9_.-]*[a-z])?$/))
-    throw new SyntaxError(`Invalid portal name: "${name}". name.match(/^_*[a-z]([a-z0-9_.-]*[a-z])?$/)`);
   const { src, params } = srcAndParams(this, name, module);
   for (let [name, value] of params.entries()) {
     const reaction = value || camelCase(name);
     const trigger = reaction.replace(/^_*[a-z]/, c => c.toUpperCase());
-    this.getRootNode().Reactions.define(name, loadDef(src, reaction));
-    this.getRootNode().Triggers.define(name, loadDef(src, trigger));
-    this.getRootNode().Reactions.defineRule(name + "_", loadRuleDefs(src, reaction));
-    this.getRootNode().Reactions.defineRule(name + ".", loadRuleDefs(src, reaction));
-    this.getRootNode().Reactions.defineRule(name + "_", loadRuleDefs(src, reaction));
-    this.getRootNode().Triggers.defineRule(name + "_", loadRuleDefs(src, trigger));
-    this.getRootNode().Triggers.defineRule(name + ".", loadRuleDefs(src, trigger));
-    this.getRootNode().Triggers.defineRule(name + "_", loadRuleDefs(src, trigger));
+    const REACTIONS = this.getRootNode().Reactions;
+    const TRIGGERS = this.getRootNode().Triggers;
+    define(REACTIONS, name, loadDef(src, reaction));
+    define(TRIGGERS, name, loadDef(src, trigger));
+    defineRule(REACTIONS, name + "-", loadRuleDefs(src, reaction));
+    defineRule(REACTIONS, name + ".", loadRuleDefs(src, reaction));
+    defineRule(REACTIONS, name + "_", loadRuleDefs(src, reaction));
+    defineRule(TRIGGERS, name + "-", loadRuleDefs(src, trigger));
+    defineRule(TRIGGERS, name + ".", loadRuleDefs(src, trigger));
+    defineRule(TRIGGERS, name + "_", loadRuleDefs(src, trigger));
   }
 }
 
-//todo cannot end with .-_
 export {
   defineReaction,
   defineTrigger,
   defineReactionRule,
   defineTriggerRule,
-  definePortal,
   definePortal as "define"
 }
