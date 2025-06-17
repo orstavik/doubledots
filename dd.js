1e163adce673c0bfe9809329c50b5126b3145f14
orstavik <orstavik77@gmail.com>
2025-06-17 10:44:19 +0200

    testing propagation

--- dd.js
+++ dd.js
@@ -1427,7 +1427,6 @@ function defineReaction(name, module) {
 
 function defineTrigger(name, module) {
   const { src, params } = srcAndParams(this, name, module);
-  debugger
   for (let [name, value] of params.entries())
     define(this.getRootNode().Triggers, name, loadDef(src, value || PascalCase(name)));
 }
